245686206a433a1dbca89157d11c06424b1d5eba
Nikita <varashilov125@gmail.com>
2024-05-15 22:42:07 +0300

    programsAndException

--- ProgramOfUserData/DataException.java
+++ ProgramOfUserData/DataException.java
@@ -1,4 +1,12 @@
 package ProgramOfUserData;
 
-public class DataException {
+public class DataException extends Exception{
+    public DataException() {
+    }
+
+    public void dataException(String i) {
+        System.out.println("Exception: DataNotCorrectFormat");
+        System.out.printf("It`s not correct format: %s", i);
+        System.out.println();
+    }
 }
--- ProgramOfUserData/GenderException.java
+++ ProgramOfUserData/GenderException.java
@@ -1,4 +1,12 @@
 package ProgramOfUserData;
 
-public class GenderException {
+public class GenderException extends Exception {
+    public GenderException() {
+    }
+
+    public void genderException(String i) {
+        System.out.println("Exception: GenderNotCorrectData");
+        System.out.printf("It`s not correct data: %s", i);
+        System.out.println();
+    }
 }
